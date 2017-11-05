ca70fe2f4eb1a75d474a491509bb04cf74d21be8
WingSeph <32689366+WingSeph@users.noreply.github.com>
2017-11-06 00:51:57 +1300

    update

--- FrameWork/board.cpp
+++
@@ -1,8 +0,0 @@
-#include "initializeboard.h"
-
-CBoard::CBoard()
-{
-	//Create Cards;
-	//Create Stacks;
-	//Allocate Cards;
-}
---
+++ FrameWork/initializeboard.cpp
@@ -0,0 +1,12 @@
+#include "initializeboard.h"
+
+CBoard::CBoard()
+{
+	//Create 52 Cards;
+	//Create main stack, temp stack, 2nd main stack, 
+	//s1,s2,s3,s4,s5,s6,s6 stack
+	//hearts stack, club stack, spades stack, diamond stack;
+
+	//Allocate Cards
+	//randomize
+}
